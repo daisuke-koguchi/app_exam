d041d66a08774ad747ce01bc365dbd9e1e93cd26
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-18 16:15:40 +0000

    Blog機能のindex.html.erbとshow.html.erbのレイアウト変更

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,6 +3,6 @@ class Blog < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favarites, source: :user 
   validates :title, presence: true, length: {minimum:1, maximum:30}
-  validates :content, presence: true, length: {minimum:1, maximum:255}e
+  validates :content, presence: true, length: {minimum:1, maximum:255}
   mount_uploader :image, ImageUploader
 end
